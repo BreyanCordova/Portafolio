c342931647823974df356686ee16484cee469f8d
Breyan Cordova <breyancordova@gmail.com>
2025-03-16 01:26:00 -0500

    Reestructuracion

---
+++ scripts/animaciones.js
@@ -0,0 +1,8 @@
+
+/*Scroll del navbar*/
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+
